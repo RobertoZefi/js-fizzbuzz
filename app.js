50f9e7a25c0d78d5e75ce0e610b47fa0ea81f3e1
RobertoZefi <zefi.roberto2001@gmail.com>
2023-02-03 09:47:13 +0100

    .

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 for(let i = 1; i <=100; i++){
-    console.log(i)
+    // console.log(i)
 
     let fizz = i % 3
     let buzz = i % 5
 
     if (fizz === 0 && buzz === 0) {
-        console.log('FizzBuzz')
+        console.log(i, 'FizzBuzz')
     } else if (buzz === 0) {
-        console.log('Buzz')
+        console.log(i, 'Buzz')
     } else if (fizz === 0){
-        console.log('Fizz')
+        console.log(i, 'Fizz')
+    }
+    else {
+        console.log(i)
     }
 
     
